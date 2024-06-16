c04a4f7bc96bd003882051cc722fa849f2e21edf
Bukowa <gitbukowa@gmail.com>
2024-06-17 00:58:29 +0200

    feat: everything changes
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

--- ck3oop-core-rs/src/lib.rs
+++ ck3oop-core-rs/src/lib.rs
@@ -1,5 +1,5 @@
 
-// This function adds two numbers.
+// This function adds two numbers.!
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
--- ck3oop-ui/src/main.ts
+++ ck3oop-ui/src/main.ts
@@ -2,5 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import {helloWorld} from "ck3oop-core-js";
 
-helloWorld("ck3oop-ui")
+helloWorld("ck3oop-u!i")
 createApp(App).mount("#app");
